bcbd03ad17880c3646f55ed3dce6fb810f219211
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-26 22:01:50 +0500

    yoyo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
     <div className='  px-5 py-5'>
     <div className='mt-5 px-5 py-10 flex justify-around border-solid border-b-2 '>
 
-        <div className='flex'>
+        <div className='flex justify-around'>
             <div className='w-1/3 flex flex-col'>
                 <img src={Logo} alt="" className='w-1/6 '/>
                 <p className='text-3xl font-semibold'>nxtBase Dynamics GmhB i.G. Kurfürstendamm 15 10715 Berlin https://www.nxtDynamics.de</p>
@@ -28,7 +28,7 @@ const Footer = () => {
                 </div>
 
             </div>
-            <div className='w-1/3 flex flex-col justify-center ml-[10%]'>
+            <div className='w-1/3 flex flex-col justify-center'>
                 <ul className='mt-10 text-3xl font-semibold'>
                 <li className='py-2 flex items-center'><AiOutlineRight/>  Home</li>
                 <li className='py-2 flex items-center'><AiOutlineRight/>  Über uns</li>
--- src/components/MessageModal.jsx
+++ src/components/MessageModal.jsx
@@ -6,7 +6,7 @@ const MessageModal = ({open, onClose}) => {
     if(!open) return null
   return (
 
-    <div className='w-screen h-screen absolute bg-white top-0 left-0'>
+    <div className='w-screen h-screen absolute bg-white top-0 left-0 md:-ml-24'>
     <div className='absolute bg-white flex'>
         <img src={PictureSeven} alt="" />
     <div onClick={onClose}><CgClose/></div>
