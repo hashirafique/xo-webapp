08d98aa3984c5b2f49ac1af80c21ee6f628281f0
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-26 11:07:08 +0500

    message bug fixed

--- src/components/DemoModal.jsx
+++ src/components/DemoModal.jsx
@@ -39,14 +39,14 @@ const DemoModal = ({open, onClose}) => {
       <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Message"/>
       
     </div>
-    </form>
+    </form></div>
     <div><Message/></div>
         
         <div onClick={onClose}><CgClose/></div>
 
         
         
-    </div>
+    
     
     </div>
   )
