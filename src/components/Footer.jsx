d8a27d14b0697b424d4f17de10643b7016b9499e
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-26 22:16:55 +0500

    hmm

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ import {AiOutlineRight} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div className='  px-5 py-5'>
-    <div className='mt-5 px-5 py-10 border-solid border-b-2 flex flex-row justify-evenly spaxe-x-20'>
+    <div className='px-5 py-5'>
+    <div className='mt-5 px-5 py-10 border-solid border-b-2'>
 
-        <div className=''>
+        <div className='flex flex-row justify-evenly spaxe-x-20'>
             <div className='w-1/3 flex flex-col'>
                 <img src={Logo} alt="" className='w-1/6 '/>
                 <p className='md:text-3xl font-semibold'>nxtBase Dynamics GmhB i.G. Kurfürstendamm 15 10715 Berlin https://www.nxtDynamics.de</p>
