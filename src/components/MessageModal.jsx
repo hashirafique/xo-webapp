7ff1e716464bbd1f8bb7f4d1461b389125a6c284
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-26 11:09:31 +0500

    bug fix

--- src/components/MessageModal.jsx
+++ src/components/MessageModal.jsx
@@ -5,7 +5,7 @@ import PictureSeven from '../assets/Picture7.png'
 const MessageModal = ({open, onClose}) => {
     if(!open) return null
   return (
-    <div className='w-full h-full bg-white flex'>
+    <div className='w-full h-full absolute bg-white flex'>
         <img src={PictureSeven} alt="" />
     <div onClick={onClose}><CgClose/></div>
     </div>
