bd40736a9e5b9ac6c937a2332fee2abf0ef60c1c
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-27 04:34:32 +0500

    hmm

--- src/components/BookADemo.jsx
+++ src/components/BookADemo.jsx
@@ -5,7 +5,7 @@ const BookADemo = () => {
     const [openModal, setOpenModal] = useState(false)
   return (
     <div>
-        <button onClick={()=> setOpenModal(true)} className='px-7 py-2 border-green-500 bg-white mt-1 rounded rounded-md text-md font-bold w-full'>Book a Demo</button>
+        <button onClick={()=> setOpenModal(true)} className='px-7 py-2 border-green-500 bg-white mt-1 rounded rounded-md text-md font-bold w-full hover:bg-gray-300 duration-300'>Book a Demo</button>
                 <DemoModal open={openModal} onClose={()=> setOpenModal(false)}/>
     </div>
   )
