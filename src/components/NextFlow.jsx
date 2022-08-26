a417c527f9bd39f62fe0fd77c66bb6869e33420e
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-26 21:31:42 +0500

    yoyo

--- src/components/NextFlow.jsx
+++ src/components/NextFlow.jsx
@@ -18,13 +18,13 @@ const NextFlow = () => {
         <img src={PictureThree} alt="" />
     </div>
         <div className='w-full md:w-6/12 bg-white flex flex-col justify-center'>
-        <h1 className='text-5xl text-gray-600 font-semibold ml-3'>nxtFLOW</h1>
-        <p className='text-3xl font-semibold uppercase ml-3 mt-5 md:w-1/2'>Perfekte Workflows in laGer und Produktion.</p>
+        <h1 className='text-5xl text-gray-600 font-semibold ml-5'>nxtFLOW</h1>
+        <p className='text-3xl font-semibold uppercase ml-3 mt-5 md:w-1/2 ml-5'>Perfekte Workflows in laGer und Produktion.</p>
         <div className='ml-3'>
-            <img src={Logo} alt="" className=' w-[12%] float-left mt-5' />
-            <p className='text-3xl mt-5 md:w-1/2'>Wir verbinden Systemwelten: Lager und Produktion. „Ware zum Mann“ und das Anbinden des Haenel Leanlifts gehört zu unserem Portfolio.</p>
+            <img src={Logo} alt="" className=' w-[12%] float-left mt-5 ml-5 pr-3'  />
+            <p className='text-3xl mt-5 md:w-1/2 ml-5'>Wir verbinden Systemwelten: Lager und Produktion. „Ware zum Mann“ und das Anbinden des Haenel Leanlifts gehört zu unserem Portfolio.</p>
         </div>
-        <div className='flex w-full mt-10 ml-10'>
+        <div className='flex w-full mt-10 ml-5'>
             <div ><button onClick={()=> setOpenModal(true)} className='bg-blue-700 text-white text-2xl w-full px-3'>Mehr</button>
                 <ModalTwo open={openModal} onClose={()=> setOpenModal(false)}/> </div>
             <div><DemoBtnOne/> </div>
