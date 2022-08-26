834d8820728bd06b38832fe7edeec8cf25ca7485
Hashir Rafique <hashir.rfq@gmail.com>
2022-08-26 12:38:50 +0500

    fix

--- src/components/PageTwo.jsx
+++ src/components/PageTwo.jsx
@@ -12,13 +12,13 @@ const PageTwo = () => {
 
       <div className="md:flex">
         <div className="w-full md:w-6/12 md:mt-14">
-          <p className="text-2xl font-semibold uppercase md:text-4xl">
+          <p className="text-2xl font-semibold uppercase md:text-4xl md:w-[85%]">
             Es gibt 1000 Augmented RealitY Entwickler Auf der Welt.
           </p>
           <p className="text-2xl font-semibold uppercase mt-2 md:mt-14">
             wir sind Anders.{" "}
           </p>
-          <p className="text-sm mt-2 md:text-lg lg:text-xl md:text-xl md:mt-14">
+          <p className="text-sm mt-2 md:text-lg lg:text-xl md:text-xl md:mt-14 md:w-[85%]">
             nxtDynamics bietet umfassende Kompetenz im Bereich Virtual-
             Augmented- und Mixed Reality, sowie Mensch-Computer-Interaktion und
             eine breite Vielfalt an Augmented Reality Applikationen,
@@ -26,7 +26,7 @@ const PageTwo = () => {
             für jede Branche, Prozess und Anwendungsbereich gezielt und einfach
             angepasst werden können.
           </p>
-          <p className="text-sm mt-2 md:text-lg lg:text-xl md:mt-14">
+          <p className="text-sm mt-2 md:text-lg lg:text-xl md:mt-14 md:w-[85%]">
             nxtDynamics ist die Kollaborationsplattform für AR Modelle,
             Simulationen und die einfachste Möglichkeit, Schritt für Schritt
             Anleitungen zu generieren, anzuzeigen, sprachgesteuert abzuarbeiten
